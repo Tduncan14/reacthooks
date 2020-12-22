583b24c49f76f05a253d63cfe97408932fe4168e
Tre'Shawn Duncan <nbatre1@yahoo.com>
2020-12-22 07:42:09 -0600

    keep moving forward

--- chapter1_2/src/App.js
+++ chapter1_2/src/App.js
@@ -3,51 +3,62 @@ import ReactDOM from 'react-dom';
 
 
 
-let value 
+let values = []
+let currentHook = 0;
 
 
 function useState(initialState){
 
 
-  if(typeof value === 'undefined') value = initialState;
+  if(typeof values[currentHook] === 'undefined') values[currentHook]= initialState;
 
 
 
+  let hookIndex = currentHook
   function setState(nextValue){
 
 
-    value = nextValue
+    values[hookIndex] = nextValue
     ReactDOM.render(<MyName /> , document.getElementById('root'));
 
   }
 
-   return [value,setState]
+   return [values[currentHook++],setState]
 }
 
 
 function MyName () {
 
 
+  currentHook = 0;
 
   const [name,setName] = useState('');
+  const [lastName,setLastName] = useState('');
 
 
 
 
       function handleChange(evt){
 
-
+         
          setName(evt.target.value);
     }
+
+      function handleLastNameChange(evt){
+
+        setLastName(evt.target.value);
+      }
 
 
     return ( 
 
         <div>
           <h1> My name is : {name}</h1>
+          <h1>My Lastname is :{lastName}</h1>
 
           <input type="text" onChange={handleChange} value={name} />
 
+         <input type="text" value={lastName} onChange={handleLastNameChange}/>i
 
 
 
